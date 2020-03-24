7de12375a566b688f552c74d8ac92ff1d165755b
Puneet Mehrotra <p.puneetmehrotra@outlook.com>
2020-03-24 11:44:05 -0700

    small test makefile arrangement

---
+++ common.mk
@@ -0,0 +1,5 @@
+WT_HOME="libs/wiredtiger"
+SRC="src"
+CXX = g++
+MAKE = make
+CXXFLAGS = -Wall -g
---
+++ src/graph_main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <string>
+
+int main (int argc, char* argv){
+    std::cout<<"This is dummy main file for testing and writing the Makefile."<<std::endl;
+}
