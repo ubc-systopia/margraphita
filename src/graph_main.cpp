59f087ba4bd9575010dd3a24759efb329ef27332
Puneet Mehrotra <p.puneetmehrotra@outlook.com>
2020-03-25 23:42:20 -0700

    some basic af error checking

--- src/graph_main.cpp
+++ src/graph_main.cpp
@@ -3,8 +3,10 @@
 #include "wiredtiger.h"
 
 static const char *home;
+using namespace std;
+
 int main (int argc, char** argv){
-    std::cout<<"This is dummy main file for testing and writing the Makefile."<<std::endl;
+    
     WT_CONNECTION *conn;
     WT_CURSOR *cursor;
     WT_SESSION *session;
@@ -12,6 +14,14 @@ int main (int argc, char** argv){
     int ret;
     /* Open a connection to the database, creating it if necessary. */
     ret = wiredtiger_open(home, NULL, "create", &conn);
+    if (ret !=0){
+        fprintf(stderr, "Failed to open connection\n");
+        return (ret);
+    }
     /* Open a session handle for the database. */
     ret = conn->open_session(conn, NULL, NULL, &session);
+    if (ret !=0){
+        fprintf(stderr, "Failed to open session\n");
+        return (ret);
+    }
 }
