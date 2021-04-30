d2d591611acbc68ead5be259c972d082da333972
Puneet Mehrotra <p.puneetmehrotra@outlook.com>
2021-04-30 10:05:37 -0700

    fixed compiler errors and moved the adj_list files into the src/ folder

---
+++ src/adj_list.cpp
@@ -0,0 +1,206 @@
+#include "adj_list.h"
+#include "adj_common.h"
+#include <filesystem>
+#include <cstring>
+#include <cassert>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <cassert>
+#include <unordered_map>
+#include <wiredtiger.h>
+
+using namespace std;
+const std::string GRAPH_PREFIX = "adj";
+
+AdjList::AdjList() {}
+
+AdjList::AdjList(opt_args opt_params)
+
+{
+    this->create_new = opt_params.create_new;
+    this->read_optimize = opt_params.read_optimize;
+    this->is_directed = opt_params.is_directed;
+    this->is_weighted = opt_params.is_weighted;
+    this->db_name = opt_params.db_name;
+
+    try
+    {
+        CommonUtil::check_graph_params(opt_params);
+    }
+    catch (GraphException G)
+    {
+        std::cout << G.what() << std::endl;
+    }
+
+    if (opt_params.create_new == false)
+    {
+        throw GraphException("Missing param " + CREATE_NEW);
+    }
+    if (opt_params.create_new)
+    {
+        create_new_graph();
+        if (opt_params.optimize_create == false)
+        {
+            /**
+       *  Create indices with graph.
+       * NOTE: If user opts to optimize graph creation, then they're
+       * responsible for calling create_indices() AFTER all the
+       * nodes/edges have been added to graph.
+       * This is done to improve performance of bulk-loading.
+       */
+        }
+    }
+    else
+    {
+        __restore_from_db(db_name); //! APT Check towards the end by a unit test
+    }
+}
+
+/**
+ * @brief Returns the metadata associated with the key param from the METADATA
+ * table.
+ * Same from standard_graph implementation
+ */
+string AdjList::get_metadata(string key)
+{
+
+    int ret = 0;
+    if (this->metadata_cursor == NULL)
+    {
+        if ((ret = _get_table_cursor(METADATA, &metadata_cursor, false)) != 0)
+        {
+            fprintf(stderr, "Failed to create cursor to the metadata table.");
+            exit(-1);
+        }
+    }
+    metadata_cursor->set_key(metadata_cursor, key.c_str());
+    ret = metadata_cursor->search(metadata_cursor);
+    if (ret != 0)
+    {
+        fprintf(stderr, "Failed to retrieve metadata for the key %s", key.c_str());
+        exit(-1);
+    }
+
+    const char *value;
+    ret = metadata_cursor->get_value(metadata_cursor, &value);
+    metadata_cursor->close(metadata_cursor); //? Should I close this?
+
+    return string(value);
+}
+
+void AdjList::create_new_graph()
+{
+    int ret;
+    // Create new directory for WT DB
+    std::filesystem::path dirname = "./db/" + db_name;
+    if (std::filesystem::exists(dirname))
+    {
+        filesystem::remove_all(dirname); // remove if exists;
+    }
+    std::filesystem::create_directories(dirname);
+
+    // open connection to WT
+    if (CommonUtil::open_connection(const_cast<char *>(dirname.c_str()), &conn) < 0)
+    {
+        exit(-1);
+    };
+
+    // Open a session handle for the database
+    if (CommonUtil::open_session(conn, &session) != 0)
+    {
+        exit(-1);
+    }
+
+    // Initialize edge ID for the edge table
+    // AdjList has no edge id in the edge table but we are using the same structure as used by Standardgraph. So, the edge_id value will be -999 for all edges in the AdjList implementation.
+    edge_id = -999;
+
+    // Set up the node table
+    // The node entry is of the form: <id>,<in_degree>,<out_degree>
+    // If the graph is read_optimized, add columns and format for in/out degrees
+    if (read_optimize)
+    {
+        node_columns.push_back(IN_DEGREE);
+        node_columns.push_back(OUT_DEGREE);
+        node_value_format = "II";
+    }
+    else
+    {
+        node_columns.push_back("na"); //have to do this because the column count must match.
+        node_value_format = "s";      //1 byte fixed length char[] to hold ""
+    }
+    // Now Create the Node Table
+    //!What happens when the table is not read-optimized? I store "" <-ok?
+
+    CommonUtil::set_table(session, NODE_TABLE, node_columns, node_key_format,
+                          node_value_format);
+
+    // ******** Now set up the Edge Table     **************
+    // Edge Column Format : <id><src><dst><weight>
+    //Now prepare the edge value format. starts with II for src,dst. Add another I if weighted
+    if (is_weighted)
+    {
+        edge_columns.push_back(WEIGHT);
+        edge_value_format += "I";
+    }
+
+    // Create edge table
+    CommonUtil::set_table(session, EDGE_TABLE, edge_columns,
+                          edge_key_format, edge_value_format);
+
+    /* Now doing the metadata table creation.
+     function This table stores the graph metadata
+     value_format:raw byte string (Su)
+     key_format: string (S)
+  */
+    string metadata_table_name = "table:" + string(METADATA);
+    if ((ret = session->create(session, metadata_table_name.c_str(),
+                               "key_format=S,value_format=S")) > 0)
+    {
+        fprintf(stderr, "Failed to create the metadata table ");
+    }
+
+    if ((ret = _get_table_cursor(METADATA, &metadata_cursor, false)) != 0)
+    {
+        fprintf(stderr, "Failed to create cursor to the metadata table.");
+        exit(-1);
+    }
+
+    // DB_NAME
+    string db_name_fmt;
+    // char *db_name_packed =
+    //     CommonUtil::pack_string_wt(db_name, session, &db_name_fmt);
+    // insert_metadata(DB_NAME, db_name_fmt, db_name_packed);
+    insert_metadata(DB_NAME, const_cast<char *>(db_name.c_str()));
+
+    // READ_OPTIMIZE
+    string read_optimized_str = read_optimize ? "true" : "false";
+    insert_metadata(READ_OPTIMIZE, const_cast<char *>(read_optimized_str.c_str()));
+
+    // IS_DIRECTED
+    string is_directed_str = is_directed ? "true" : "false";
+    insert_metadata(IS_DIRECTED, const_cast<char *>(is_directed_str.c_str()));
+
+    //IS_WEIGHTED
+    string is_weighted_str = is_weighted ? "true" : "false";
+    insert_metadata(IS_WEIGHTED, const_cast<char *>(is_weighted_str.c_str()));
+
+    this->metadata_cursor->close(this->metadata_cursor);
+}
+
+/**
+ * @brief This private function inserts metadata values into the metadata
+ * table. The fields are self explanatory.
+ * Same from standard_graph implementation.
+ */
+void AdjList::insert_metadata(string key, char *value)
+{
+    this->metadata_cursor->set_key(metadata_cursor, key.c_str());
+    this->metadata_cursor->set_value(metadata_cursor, value);
+    if (this->metadata_cursor->insert(metadata_cursor) != 0)
+    {
+        fprintf(stderr, "failed to insert metadata for key %s", key.c_str());
+        // TODO(puneet): Maybe create a GraphException?
+    }
+}
---
+++ src/adj_list.h
@@ -0,0 +1,113 @@
+#ifndef STD_GRAPH
+#define STD_GRAPH
+
+#include "common.h"
+#include "graph_exception.h"
+#include <filesystem>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <wiredtiger.h>
+
+using namespace std;
+
+class AdjList
+{
+public:
+    AdjList(opt_args opt_params);
+    AdjList();
+
+    void create_new_graph();
+
+    void add_node(node to_insert);
+    bool has_node(int node_id);
+    node get_node(int node_id);
+    void delete_node(int node_id);
+    node get_random_node();
+    int get_in_degree(int node_id);
+    int get_out_degree(int node_id);
+    std::vector<node> get_nodes();
+
+    int get_num_nodes();
+    int get_num_edges();
+
+    void add_edge(edge to_insert);
+    bool has_edge(int src_id, int dst_id);
+    int get_edge_weight(int src_id, int dst_id);
+    void delete_edge(int src_id, int dst_id);
+    void update_edge_weight(int src_id, int dst_id, int edge_weight);
+
+    void add_adjlist(WT_CURSOR *cursor, int node_id);
+    void delete_adjlist(WT_CURSOR *cursor, int node_id);
+    void delete_node_from_adjlists(int node_id);
+    std::vector<int> get_adjlist(WT_CURSOR *cursor, int node_id);
+    void add_to_adjlists(WT_CURSOR *cursor, int node_id, int to_insert);
+    void delete_from_adjlists(WT_CURSOR *cursor, int node_id, int to_delete);
+
+    void delete_related_edges(WT_CURSOR *index_cursor, int node_id);
+
+    void update_node_degree(WT_CURSOR *cursor, int node_id, int in_degree,
+                            int out_degree);
+    edge get_edge(int src_id, int dst_id);
+    // void update_edge(int src_id, int dst_id, char *new_attrs); <-not needed.
+    // skip.
+    std::vector<edge> get_edges();
+    std::vector<edge> get_out_edges(int node_id);
+    std::vector<node> get_out_nodes(int node_id);
+    std::vector<edge> get_in_edges(int node_id);
+    std::vector<node> get_in_nodes(int node_id);
+    //! void set_node_data(int node_id, int idx, string data);
+    //! Do we need get all weights of in/out degree from a node
+    std::string get_node_data(int node_id, int idx);
+    void get_node_iter(); //! APT Check
+    void get_edge_iter(); //! APT Check
+    // Metadata operations:
+    void insert_metadata(string key, char *value);
+    string get_metadata(string key);
+    void close();
+
+    // WT privates
+private:
+    WT_CONNECTION *conn;
+    WT_SESSION *session;
+    //create params`
+    bool create_new = true;
+    bool read_optimize = true;
+    bool is_directed = true;
+    bool is_weighted = true; //needed to understand when to interpret the weight field in struct edge
+    std::string db_name;
+
+    //structure of the graph
+    int edge_id;
+    int node_attr_size = 0; // set on checking the list len
+
+    vector<string> node_columns = {ID}; //Always there :)
+    vector<string> edge_columns = {SRC, DST};
+    string node_value_format;
+    string node_key_format = "I";
+    string edge_key_format = "II";  // SRC DST in the edge table
+    string edge_value_format = "I"; // Edge Weight in the edge table
+
+    WT_CURSOR *node_cursor = NULL;
+    WT_CURSOR *random_node_cursor = NULL;
+    WT_CURSOR *edge_cursor = NULL;
+    WT_CURSOR *src_dst_index_cursor = NULL; //! APT Check
+    WT_CURSOR *src_index_cursor = NULL;
+    WT_CURSOR *dst_index_cursor = NULL;
+    WT_CURSOR *metadata_cursor = NULL;
+
+    void __node_to_record(WT_CURSOR *cursor, node to_insert);  //! APT Check
+    node __record_to_node(WT_CURSOR *cursor);                  //! APT Check
+    edge __record_to_edge(WT_CURSOR *cursor);                  //! APT Check
+    void __read_from_edge_idx(WT_CURSOR *cursor, edge *e_idx); //! APT Check
+
+    // Internal cursor methods
+    int _get_table_cursor(string table, WT_CURSOR **cursor, bool is_random); //! APT Check
+    int _get_index_cursor(std::string table_name, std::string idx_name,
+                          std::string projection, WT_CURSOR **cursor); //! APT Check
+    void __restore_from_db(string db_name);
+    void create_indices();
+    void drop_indices();
+};
+
+#endif
