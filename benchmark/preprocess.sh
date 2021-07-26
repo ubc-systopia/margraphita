ba9c15a581813372e4fc5b7994b67ac133449226
Puneet Mehrotra <p.puneetmehrotra@outlook.com>
2021-07-26 08:58:37 -0700

    changed preprocessing script to have the options actually make sense

--- benchmark/preprocess.sh
+++ benchmark/preprocess.sh
@@ -2,6 +2,10 @@
 set -x
 set -e
 usage() { echo "Usage: $0 [-f <dataset path> -o <output path> -n <dataset name>" 1>&2; exit 1; }
+#dataset path : the absolute path to the dataset that is being inserted. 
+# output path: absolute path to the directory that should contain the output
+# files
+#dataset name : used to construct the DB name and to name the output files
 
 while getopts "f:o:n:" o; do
     case "${o}" in
@@ -24,28 +28,29 @@ done
 sed -i '/^#/d' ${filename}
 
 ##Split the edges file
-split --number=l/10 ${filename} "${output}_edges"
+split --number=l/10 ${filename} "${output}/${dataset}_edges"
 
 ##Process out degrees: count of unique entries in 1st column is the outdegree
 #for each node.
 cut -f1 ${filename} | sort | uniq -c > "temp_nodes_outdeg.txt"
 #To remove leading whitespace
-sed 's/^ *//' < "temp_nodes_outdeg.txt" > "${output}_nodes_outdeg_"
+sed 's/^ *//' < "temp_nodes_outdeg.txt" > "${output}/${dataset}_nodes_outdeg_"
 #split into 10 parts for MT ingest
-#split --number=l/10 "${output}_nodes_outdeg_" "${output}_nodes_outdeg_"
+#split --number=l/10 "${output}/${dataset}_nodes_outdeg_" "${output}/${dataset}_nodes_outdeg_"
 
 ##Process in degrees: count of unique entries in 2nd column is the indegree for
 #each node
 cut -f2 ${filename} | sort | uniq -c > "temp_nodes_indeg.txt"
 #To remove leading whitespace
-sed 's/^ *//' < "temp_nodes_indeg.txt" > "${output}_nodes_indeg_"
+sed 's/^ *//' < "temp_nodes_indeg.txt" > "${output}/${dataset}_nodes_indeg_"
 #split into 10 parts for MT ingest
-#split --number=l/10 "${output}_nodes_indeg_" "${output}_nodes_indeg_"
+#split --number=l/10 "${output}/${dataset}_nodes_indeg_" "${output}/${dataset}_nodes_indeg_"
 
-#Now create an empty DB to initialize insert
-#using pagerank for this. Too lazy to do any better :(
+#Now create an empty DBs for all three representations for  insertion
+#Using pagerank for this. Too lazy to do any better :(
 ./pagerank -n -m std_${dataset} -b PR -a ${filename} -s ${dataset} -o -r -d -l std -e
 
 ./pagerank -n -m adj_${dataset} -b PR -a ${filename} -s ${dataset} -o -r -d -l adjlist -e
 
 ./pagerank -n -m ekey_${dataset} -b PR -a ${filename} -s ${dataset} -o -r -d -l edgekey -e
+# Here - 
