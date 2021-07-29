628e9ff1b633fee9e90bc8e7053df5f481cbe2dc
Puneet Mehrotra <p.puneetmehrotra@outlook.com>
2021-07-28 18:04:13 -0700

    there's a bug in the ekey insertion code. saving state for the day

--- benchmark/bulk_insert.cpp
+++ benchmark/bulk_insert.cpp
@@ -20,8 +20,10 @@ WT_CONNECTION *conn;
 double num_edges;
 double num_nodes;
 std::string dataset;
-int edge_per_part;
+int edge_per_part, node_per_part;
 int read_optimized = 0;
+int is_directed = 1;
+std::string type;
 
 typedef struct thread_params
 {
@@ -52,8 +54,24 @@ void *insert_edge_thread(void *arg)
 
     for (edge e : edjlist)
     {
-        cursor->set_key(cursor, start_idx);
-        cursor->set_value(cursor, e.src_id, e.dst_id, 0);
+
+        if (type == "ekey")
+        {
+            cursor->set_key(cursor, e.src_id, e.dst_id);
+            cursor->set_value(cursor, "");
+        }
+        else if (type == "std")
+        {
+            cursor->set_key(cursor, start_idx);
+            cursor->set_value(cursor, e.src_id, e.dst_id, 0);
+        }
+        else if (type == "adj")
+        {
+
+            cursor->set_key(cursor, e.src_id, e.dst_id);
+            cursor->set_value(cursor, e.src_id, e.dst_id, 0);
+        }
+
         cursor->insert(cursor);
         start_idx++;
     }
@@ -75,26 +93,87 @@ int insert_node()
     WT_SESSION *session;
 
     conn->open_session(conn, NULL, NULL, &session);
-    session->open_cursor(session, "table:node", NULL, NULL, &cursor);
+    if (type == "ekey")
+    {
+        session->open_cursor(session, "table:edge", NULL, NULL, &cursor);
+    }
+    else
+    {
+        session->open_cursor(session, "table:node", NULL, NULL, &cursor);
+    }
     int cnt = 0;
     for (auto &entry : nodelist)
     {
         node n = entry.second;
-        cursor->set_key(cursor, n.id);
-        if (read_optimized)
+        if (type == "ekey")
         {
-            cursor->set_value(cursor, n.in_degree, n.out_degree);
+            cursor->set_key(cursor, n.id, -1);
+            cursor->set_value(cursor, "");
         }
         else
         {
-            cursor->set_value(cursor, 0, 0);
-        }
+            cursor->set_key(cursor, n.id);
 
-        cursor->insert(cursor);
-        cnt++;
+            if (read_optimized)
+            {
+                cursor->set_value(cursor, n.in_degree, n.out_degree);
+            }
+            else
+            {
+                cursor->set_value(cursor, 0, 0);
+            }
+
+            cursor->insert(cursor);
+            cnt++;
+        }
     }
     std::cout << "inserted into nodes :" << cnt << std::endl;
     return size;
+}
+
+void *insert_inadjlist(void *arg)
+{
+    WT_CURSOR *cursor;
+    WT_SESSION *session;
+    int tid = *(int *)arg;
+    int start_idx = (node_per_part * tid) + 1;
+    int end_idx = (node_per_part * (tid + 1));
+
+    conn->open_session(conn, NULL, NULL, &session);
+    session->open_cursor(session, "table:adjlistin", NULL, NULL, &cursor);
+    int cnt = 0;
+    for (auto &entry : reader::in_adjlist)
+    {
+        int node_id = entry.first;
+        int indeg = entry.second.size();
+        size_t size;
+        std::string packed_adjlist = CommonUtil::pack_int_vector_std(entry.second, &size);
+        cursor->set_key(cursor, node_id);
+        cursor->set_value(cursor, indeg, packed_adjlist);
+    }
+    cursor->close(cursor);
+    session->close(session, NULL);
+}
+
+void *insert_outadjlist(void *arg)
+{
+    WT_CURSOR *cursor;
+    WT_SESSION *session;
+
+    conn->open_session(conn, NULL, NULL, &session);
+    session->open_cursor(session, "table:adjlistout", NULL, NULL, &cursor);
+    int cnt = 0;
+    for (auto &entry : reader::in_adjlist)
+    {
+        int node_id = entry.first;
+        int outdeg = entry.second.size();
+        size_t size;
+        std::string packed_adjlist = CommonUtil::pack_int_vector_std(entry.second, &size);
+        cursor->set_key(cursor, node_id);
+        cursor->set_value(cursor, outdeg, packed_adjlist);
+    }
+    cursor->close(cursor);
+    session->close(session, NULL);
 }
 
 int main(int argc, char *argv[])
@@ -105,11 +184,12 @@ int main(int argc, char *argv[])
         {"edges", required_argument, 0, 'e'},
         {"nodes", required_argument, 0, 'n'},
         {"file", required_argument, 0, 'f'},
+        {"undirected", required_argument, &is_directed, 'u'},
         {"ropt", required_argument, &read_optimized, 'r'}};
     int option_idx = 0;
     int c;
 
-    while ((c = getopt_long(argc, argv, "d:e:n:f:r", long_opts, &option_idx)) != -1)
+    while ((c = getopt_long(argc, argv, "d:e:n:f:ru", long_opts, &option_idx)) != -1)
     {
         switch (c)
         {
@@ -128,47 +208,115 @@ int main(int argc, char *argv[])
         case 'r':
             read_optimized = 1;
             break;
+        case 'u':
+            is_directed = 0;
+            break;
         case ':':
         /* missing option argument */
         case '?':
         default:
-            std::cout << "enter dbname(--db), edgecount (--edges), nodecount (--nodes), and dataset file (--file)";
+            std::cout << "enter dbname base name(--db), edgecount (--edges), nodecount (--nodes), and dataset file (--file), undirected (--undirected), read_opt (--ropt)";
             exit(0);
         }
     }
     edge_per_part = ceil(num_edges / NUM_THREADS);
+
     std::cout << "edges per part : " << edge_per_part << std::endl;
-    wiredtiger_open(db_name.c_str(), NULL, "create", &conn);
 
-    int i;
-    pthread_t threads[NUM_THREADS];
-
-    //Insert Edges First;
-    std::cout << "id \t filename \t starting index\t ending idx\t size" << std::endl;
-    auto start = std::chrono::steady_clock::now();
-    for (i = 0; i < NUM_THREADS; i++)
+    for (std::string prefix : {"ekey"}) //"std", "adj",
     {
-        pthread_create(&threads[i], NULL, insert_edge_thread, new int(i));
-    }
-    int edge_total = 0;
-    int node_total = 0;
-    for (i = 0; i < NUM_THREADS; i++)
-    {
-        void *found;
-        pthread_join(threads[i], &found);
-        edge_total = edge_total + (intptr_t)found;
-    }
-    auto end = std::chrono::steady_clock::now();
-    std::cout << "Edges inserted in " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << std::endl;
+        type = prefix;
+        std::string middle;
+        if (read_optimized)
+        {
+            middle += "r";
+        }
+        if (is_directed)
+        {
+            middle += "d";
+        }
 
-    //Now insert nodes;
-    start = std::chrono::steady_clock::now();
-    node_total = insert_node();
-    end = std::chrono::steady_clock::now();
-    std::cout << "Nodes inserted in " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << std::endl;
+        std::string _db_name = "db/" + prefix + "_" + middle + "_" + db_name;
+        if (wiredtiger_open(_db_name.c_str(), NULL, "create", &conn) != 0)
+        {
+            std::cout << "Could not open the DB: " << _db_name;
+            exit(1);
+        }
+        int i;
+        pthread_t threads[NUM_THREADS];
 
-    std::cout << "total number of edges inserted = " << edge_total << std::endl;
-    std::cout << "total number of nodes inserted = " << node_total << std::endl;
-    conn->close(conn, NULL);
+        //Insert Edges First;
+        std::cout << "id \t filename \t starting index\t ending idx\t size" << std::endl;
+        auto start = std::chrono::steady_clock::now();
+        for (i = 0; i < NUM_THREADS; i++)
+        {
+            pthread_create(&threads[i], NULL, insert_edge_thread, new int(i));
+        }
+        int edge_total = 0;
+        int node_total = 0;
+        // for (i = 0; i < NUM_THREADS; i++)
+        // {
+        //     void *found;
+        //     pthread_join(threads[i], &found);
+        //     edge_total = edge_total + (intptr_t)found;
+        // }
+        auto end = std::chrono::steady_clock::now();
+        // std::cout << "Edges inserted in " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << std::endl;
+
+        //Now insert nodes;
+        start = std::chrono::steady_clock::now();
+        node_total = insert_node();
+        end = std::chrono::steady_clock::now();
+        std::cout << "Nodes inserted in " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << std::endl;
+
+        //Now create the adjlist, if needed
+        //node_per_part = ceil(num_nodes / NUM_THREADS);
+
+        std::cout << reader::in_adjlist.bucket_count() << "\t" << reader::out_adjlist.bucket_count();
+
+        // if (prefix == "adj")
+        // {
+        //     //insert in_Adjlist first
+        //     start = std::chrono::steady_clock::now();
+        //     int in_adj_total = 0, out_adj_total = 0;
+        //     for (i = 0; i < NUM_THREADS; i++)
+        //     {
+        //         pthread_create(&threads[i], NULL, insert_inadjlist, new int(i));
+        //     }
+
+        //     for (i = 0; i < NUM_THREADS; i++)
+        //     {
+        //         void *found;
+        //         pthread_join(threads[i], &found);
+        //         in_adj_total += in_adj_total + (intptr_t)found;
+        //     }
+
+        //     end = std::chrono::steady_clock::now();
+        //     std::cout << "InAdjList inserted in " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << std::endl;
+
+        //     //Insert out_Adjlist now
+        //     start = std::chrono::steady_clock::now();
+        //     int in_adj_total = 0, out_adj_total = 0;
+        //     for (i = 0; i < NUM_THREADS; i++)
+        //     {
+        //         pthread_create(&threads[i], NULL, insert_outadjlist, new int(i));
+        //     }
+
+        //     for (i = 0; i < NUM_THREADS; i++)
+        //     {
+        //         void *found;
+        //         pthread_join(threads[i], &found);
+        //         out_adj_total += out_adj_total + (intptr_t)found;
+        //     }
+
+        //     end = std::chrono::steady_clock::now();
+        //     std::cout << "OutAdjList inserted in " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << std::endl;
+        // }
+
+        std::cout << "total number of edges inserted = " << edge_total << std::endl;
+        std::cout << "total number of nodes inserted = " << node_total << std::endl;
+        conn->close(conn, NULL);
+    }
+
     return (EXIT_SUCCESS);
 }
