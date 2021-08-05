1621a3de9b196da38ff711b1c713ca84d610fb1e
Puneet Mehrotra <p.puneetmehrotra@outlook.com>
2021-08-05 11:17:15 -0700

    left out this file by accident

---
+++ benchmark/reader.h
@@ -0,0 +1,18 @@
+#ifndef READER_H
+#define READER_H
+#include <vector>
+#include <string>
+#include "common.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <mutex>
+#include "bulk_insert.h"
+namespace reader
+{
+
+    std::vector<edge> parse_edge_entries(std::string filename);
+    std::unordered_map<int, node> parse_node_entries(std::string filename);
+}
+
+#endif
