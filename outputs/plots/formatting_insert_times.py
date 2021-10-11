30277b0eda94244167699365910f9e3e60b0ab7d
Puneet Mehrotra <p.puneetmehrotra@outlook.com>
2021-10-10 23:24:44 -0700

    changes path

--- outputs/plots/formatting_insert_times.py
+++ outputs/plots/formatting_insert_times.py
@@ -5,13 +5,16 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
+import os
 
 
 def compute_speed(row):
     return(row["Disk_Space"]*1.0/row["Real_Time"])
 
+
 def main():
-    df = pd.read_csv("/Users/amee/Desktop/Research/Puneet_Graphs/formatting_insert_time.csv")
+    cwd = os.getcwd()
+    df = pd.read_csv(cwd + "/outputs/plots/formatting_insert_time.csv")
     print(list(df))
     print(df)
     df["Disk_Space"] = 50000
@@ -22,7 +25,7 @@ def main():
     print(df)
 
     # Average speed of the ssd
-    ssd_speed = [360,359,363,359]
+    ssd_speed = [360, 359, 363, 359]
     avg_speed = sum(ssd_speed)/len(ssd_speed)
     print(avg_speed)
 
@@ -35,7 +38,7 @@ def main():
     print(x)
     y = [avg_speed]*len(x)
     print(y)
-    plt.plot(x,y, linestyle="-.", label="SSD Baseline", color="green")
+    plt.plot(x, y, linestyle="-.", label="SSD Baseline", color="green")
 
     plt.legend()
     plt.xlabel("S Value", fontsize=18)
@@ -45,6 +48,7 @@ def main():
     plt.show()
 
     return(0)
+
 
 if __name__ == "__main__":
     main()
