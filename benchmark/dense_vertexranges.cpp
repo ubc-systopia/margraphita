db7b3965dc06c91809f0b8186055f9f1658e4e45
Puneet Mehrotra <p.puneetmehrotra@outlook.com>
2022-05-16 11:34:24 -0700

    fixed directory location issue in dense_vertexmap and changed ints to be int64_t

--- benchmark/dense_vertexranges.cpp
+++ benchmark/dense_vertexranges.cpp
@@ -19,7 +19,7 @@ double num_edges;
 double num_nodes;
 std::string dataset;
 std::string out_dir;
-std::unordered_map<int, int> remap;
+std::unordered_map<int64_t, int64_t> remap;
 
 typedef std::shared_mutex Lock;
 typedef std::unique_lock<Lock> WriteLock;
@@ -27,14 +27,14 @@ typedef std::shared_lock<Lock> ReadLock;
 
 Lock lock;
 
-void construct_vertex_mapping(int line_num)
+void construct_vertex_mapping(int64_t line_num)
 {
     std::string filename = dataset + "_nodes";
     std::ifstream nodefile(filename.c_str());
     if (nodefile.is_open())
     {
         std::string tp;
-        int line = 0;
+        int64_t line = 0;
         while (getline(nodefile, tp))
         {
             if (line < line_num)
@@ -44,7 +44,7 @@ void construct_vertex_mapping(int line_num)
             }
             else
             {
-                int val;
+                int64_t val;
                 std::stringstream s_str(tp);
                 s_str >> val;
                 WriteLock writer(lock);
@@ -60,19 +60,19 @@ void construct_vertex_mapping(int line_num)
         std::cout << "failed;";
     }
 }
-void convert_edge_list(int beg_offset, int end_offset, int t_id)
+void convert_edge_list(int64_t beg_offset, int64_t end_offset, int64_t t_id)
 {
     std::ifstream edgefile(dataset.c_str());
 
     char c = (char)(97 + t_id);
-    std::string out_filename = dataset + "edges";
+    std::string out_filename = dataset + "_edges";
     out_filename.push_back('a');
     out_filename.push_back(c);
     std::ofstream outfile(out_filename.c_str());
     if (edgefile.is_open())
     {
         std::string tp;
-        int line = 0;
+        int64_t line = 0;
         while (getline(edgefile, tp))
         {
             if (line < beg_offset)
@@ -82,7 +82,7 @@ void convert_edge_list(int beg_offset, int end_offset, int t_id)
             }
             else if (line >= beg_offset && line < end_offset)
             {
-                int src, dst;
+                int64_t src, dst;
                 std::stringstream s_str(tp);
                 s_str >> src;
                 s_str >> dst;
@@ -149,7 +149,7 @@ int main(int argc, char *argv[])
 #pragma omp parallel for num_threads(NUM_THREADS) shared(num_nodes)
     for (int i = 0; i < NUM_THREADS; i++)
     {
-        int offset = ((i * num_nodes) / NUM_THREADS);
+        int64_t offset = ((i * num_nodes) / NUM_THREADS);
 
         construct_vertex_mapping(offset);
     }
@@ -165,8 +165,8 @@ int main(int argc, char *argv[])
 #pragma omp parallel for num_threads(NUM_THREADS) shared(num_edges)
     for (int i = 0; i < NUM_THREADS; i++)
     {
-        int beg_offset = ((i * num_edges) / NUM_THREADS);
-        int end_offset = ((i + 1) * num_edges) / NUM_THREADS;
+        int64_t beg_offset = ((i * num_edges) / NUM_THREADS);
+        int64_t end_offset = ((i + 1) * num_edges) / NUM_THREADS;
         convert_edge_list(beg_offset, end_offset, i);
     }
     return (EXIT_SUCCESS);
