d7acfe2dfd5f47ed2d09576e6dcb0682b03e4629
apricot2012 <haotiangong@hotmail.com>
2022-05-18 10:50:14 -0700

    Fixed according to comments, added constant BUFFER_LENGTH

--- benchmark/bulk_insert.cpp
+++ benchmark/bulk_insert.cpp
@@ -78,7 +78,6 @@ void print_time_csvline(std::string db_name,
 void insert_stats_to_session(WT_SESSION *session, int edgeNo, int nodeNo)
 {
     WT_CURSOR *cursor;
-<<<<<<< HEAD
     if (session->open_cursor(session, "table:metadata", NULL, NULL, &cursor) !=
         0)
     {
@@ -91,17 +90,18 @@ void insert_stats_to_session(WT_SESSION *session, int edgeNo, int nodeNo)
     cursor->insert(cursor);
 
     sprintf(buffer, "%d", edgeNo);
-=======
-    if (int ret = session->open_cursor(
-                      session, "table:metadata", NULL, NULL, &cursor) != 0)
+    if (session->open_cursor(session, "table:metadata", NULL, NULL, &cursor) !=
+        0)
     {
         std::cout << "Failed to open metadata table";
     }
+    char buffer[BUFFER_LENGTH];
+    sprintf(buffer, "%d", nodeNo);
     cursor->set_key(cursor, node_count.c_str());
-    cursor->set_value(cursor, std::to_string(nodeNo).c_str());
+    cursor->set_value(cursor, buffer);
     cursor->insert(cursor);
 
->>>>>>> Fixed issue by opening cursor to metadata table & updating counts
+    sprintf(buffer, "%d", edgeNo);
     cursor->set_key(cursor, edge_count.c_str());
     cursor->set_value(cursor, std::to_string(edgeNo).c_str());
     cursor->insert(cursor);
