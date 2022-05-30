4f5bbd422451e24d35b1548fdbf04fd7f98d347c
Puneet Mehrotra <p.puneetmehrotra@outlook.com>
2022-05-30 13:39:41 -0700

    fixed a bug in how we were creating the graph objects. Added a GraphException there

--- src/GraphCreate.h
+++ src/GraphCreate.h
@@ -2,33 +2,31 @@
 #include "common.h"
 #include "edgekey.h"
 #include "graph.h"
+#include "graph_exception.h"
 #include "standard_graph.h"
 
 class GraphFactory
 {
    public:
-    GraphBase *CreateGraph(graph_opts opts)
+    GraphBase *CreateGraph(graph_opts &opts)
     {
         GraphBase *ptr = nullptr;
         if (opts.type == GraphType::Std)
         {
             ptr = new StandardGraph(opts);
+            return ptr;
         }
         else if (opts.type == GraphType::Adj)
         {
             ptr = new AdjList(opts);
+            return ptr;
         }
         else if (opts.type == GraphType::EKey)
         {
             ptr = new EdgeKey(opts);
-        }
-        if (!ptr)
-        {
             return ptr;
         }
-        else
-        {
-            throw "Failed to create graph object";
-        }
+
+        throw GraphException("Failed to create graph object");
     }
 };
