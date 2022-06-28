6bb728b92966fc775f96b66840147727824cfb53
Jack Li <jackli@leap-503.nss.cs.ubc.ca>
2022-06-28 16:59:09 -0700

    added locks in GraphBase, usage of locks not yet implemented, implemented create_graph_handle()

--- src/graph_engine.cpp
+++ src/graph_engine.cpp
@@ -2,3 +2,39 @@
 
 using namespace std;
 
+GraphEngine::GraphEngine(graph_engine_opts engine_opts)
+{
+    num_threads = engine_opts.num_threads;
+    opts = engine_opts.num_threads;
+    locks = new LockSet();
+}
+
+GraphEngine::~GraphEngine()
+{
+    delete locks;
+}
+
+GraphBase* GraphEngine::create_graph_handle()
+{
+    GraphBase *ptr = nullptr;
+        if (opts.type == GraphType::Std)
+        {
+            ptr = new StandardGraph(opts);
+            ptr->set_locks(locks);
+            return ptr;
+        }
+        else if (opts.type == GraphType::Adj)
+        {
+            ptr = new AdjList(opts);
+            ptr->set_locks(locks);
+            return ptr;
+        }
+        else if (opts.type == GraphType::EKey)
+        {
+            ptr = new EdgeKey(opts);
+            ptr->set_locks(locks);
+            return ptr;
+        }
+
+        throw GraphException("Failed to create graph object");
+}
--- src/graph_engine.h
+++ src/graph_engine.h
@@ -1,20 +1,36 @@
 #ifndef GRAPH_ENGINE
 #define GRAPH_ENGINE
 
-#include "lock.h"
 #include "graph.h"
+
+#include "adj_list.h"
+#include "edgekey.h"
+#include "standard_graph.h"
+
+#include "common.h"
+#include "lock.h"
+#include "graph_exception.h"
 
 using namespace std;
 
 class GraphEngine
 {
+
+    struct graph_engine_opts
+    {
+        int num_threads;
+        graph_opts opts;
+    };
+
     public:
-    GraphEngine();
+    GraphEngine(graph_engine_opts engine_opts);
     GraphBase* create_graph_handle();
-    
+
 
     private:
     LockSet* locks;
+    int num_threads;
+    graph_opts opts;
 };
 
 #endif
