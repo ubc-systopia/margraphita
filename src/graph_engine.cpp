2c6d0ac0ad5660ec411317d16f36b33ccbff0167
Jack Li <jackli@leap-503.nss.cs.ubc.ca>
2022-06-28 17:22:09 -0700

    adapted changes from parallel_impl

--- src/graph_engine.cpp
+++ src/graph_engine.cpp
@@ -5,7 +5,8 @@ using namespace std;
 GraphEngine::GraphEngine(graph_engine_opts engine_opts)
 {
     num_threads = engine_opts.num_threads;
-    opts = engine_opts.num_threads;
+    opts = engine_opts.opts;
+    check_opts_valid();
     locks = new LockSet();
 }
 
@@ -37,4 +38,32 @@ GraphBase* GraphEngine::create_graph_handle()
         }
 
         throw GraphException("Failed to create graph object");
+}
+
+void GraphEngine::check_opts_valid()
+{
+    try
+    {
+        CommonUtil::check_graph_params(opts);
+    }
+    catch (GraphException &G)
+    {
+        std::cout << G.what() << std::endl;
+    }
+    if (!CommonUtil::check_dir_exists(opts.stat_log))
+    {
+        std::filesystem::create_directories(opts.stat_log);
+    }
+    if (opts.create_new)
+    {
+        std::string dirname = opts.db_dir + "/" + opts.db_name;
+        CommonUtil::create_dir(dirname);
+        if (CommonUtil::open_connection(const_cast<char *>(dirname.c_str()),
+                                        opts.stat_log,
+                                        opts.conn_config,
+                                        &conn) < 0)
+        {
+            throw GraphException("Cannot open connection to new DB");
+        };
+    }
 }
