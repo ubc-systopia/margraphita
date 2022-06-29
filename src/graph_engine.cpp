f6f50d53c472076b583805f4d6807085174e25b2
Jack Li <jackli@leap-503.nss.cs.ubc.ca>
2022-06-28 17:30:40 -0700

    included checks for num_threads

--- src/graph_engine.cpp
+++ src/graph_engine.cpp
@@ -10,38 +10,40 @@ GraphEngine::GraphEngine(graph_engine_opts engine_opts)
     locks = new LockSet();
 }
 
-GraphEngine::~GraphEngine()
-{
-    delete locks;
-}
+GraphEngine::~GraphEngine() { delete locks; }
 
-GraphBase* GraphEngine::create_graph_handle()
+GraphBase *GraphEngine::create_graph_handle()
 {
     GraphBase *ptr = nullptr;
-        if (opts.type == GraphType::Std)
-        {
-            ptr = new StandardGraph(opts);
-            ptr->set_locks(locks);
-            return ptr;
-        }
-        else if (opts.type == GraphType::Adj)
-        {
-            ptr = new AdjList(opts);
-            ptr->set_locks(locks);
-            return ptr;
-        }
-        else if (opts.type == GraphType::EKey)
-        {
-            ptr = new EdgeKey(opts);
-            ptr->set_locks(locks);
-            return ptr;
-        }
+    if (opts.type == GraphType::Std)
+    {
+        ptr = new StandardGraph(opts);
+        ptr->set_locks(locks);
+        return ptr;
+    }
+    else if (opts.type == GraphType::Adj)
+    {
+        ptr = new AdjList(opts);
+        ptr->set_locks(locks);
+        return ptr;
+    }
+    else if (opts.type == GraphType::EKey)
+    {
+        ptr = new EdgeKey(opts);
+        ptr->set_locks(locks);
+        return ptr;
+    }
 
-        throw GraphException("Failed to create graph object");
+    throw GraphException("Failed to create graph object");
 }
 
 void GraphEngine::check_opts_valid()
 {
+    if (num_threads < 1)
+    {
+        throw GraphException("Number of threads must be at least 1");
+    }
+
     try
     {
         CommonUtil::check_graph_params(opts);
