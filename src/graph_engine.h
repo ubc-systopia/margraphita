6081592315b07fbb4b025ac9a661ed39384cd23a
Jack Li <jackli@leap-503.nss.cs.ubc.ca>
2022-07-05 13:40:28 -0700

    fixed merge conflicts

--- src/graph_engine.h
+++ src/graph_engine.h
@@ -23,16 +23,19 @@ class GraphEngine
     GraphEngine(graph_engine_opts engine_opts);
     ~GraphEngine();
     GraphBase* create_graph_handle();
-    void close_connection();
+    void close_graph();  // TODO
 
-   private:
-    void check_opts_valid();
-    void create_new_graph();
-    void open_connection();
+   protected:
     WT_CONNECTION* conn = nullptr;
     LockSet* locks;
     int num_threads;
     graph_opts opts;
+
+    void check_opts_valid();
+    void create_new_graph();
+    void open_connection();
+    void close_connection();
+    WT_CONNECTION* get_connection();
 };
 
 #endif
