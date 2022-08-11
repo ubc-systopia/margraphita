a8623adec61f332c55e2770b593e9090e0a82033
Jack Li <jackli@leap-503.nss.cs.ubc.ca>
2022-08-10 22:34:45 -0700

    added reference to betweeness centrality paper

---
+++ benchmark/bc.cpp
@@ -0,0 +1,25 @@
+#include <math.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include <algorithm>
+#include <cassert>
+#include <deque>
+#include <fstream>
+#include <iostream>
+#include <set>
+#include <vector>
+
+#include "adj_list.h"
+#include "command_line.h"
+#include "common.h"
+#include "edgekey.h"
+#include "graph_engine.h"
+#include "graph_exception.h"
+#include "standard_graph.h"
+#include "times.h"
+
+/**
+ * Betweeness Centrality - Vertex Centric
+ * http://snap.stanford.edu/class/cs224w-readings/brandes01centrality.pdf
+ */
