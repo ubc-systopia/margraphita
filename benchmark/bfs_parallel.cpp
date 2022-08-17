4227bda163b7edabb6d809a6ddcc4003083d9fab
Jack Li <jackli@leap-503.nss.cs.ubc.ca>
2022-08-17 15:03:32 -0700

    debugged bfs_parallel

--- benchmark/bfs_parallel.cpp
+++ benchmark/bfs_parallel.cpp
@@ -25,7 +25,7 @@
 #include "standard_graph.h"
 #include "times.h"
 
-int64_t BUStep(const GraphEngine &graph_engine,
+int64_t BUStep(GraphEngine *graph_engine,
                pvector<node_id_t> &parent,
                Bitmap &front,
                Bitmap &next,
@@ -37,11 +37,11 @@ int64_t BUStep(const GraphEngine &graph_engine,
 #pragma omp parallel for reduction(+ : awake_count) num_threads(thread_num)
     for (int i = 0; i < thread_num; i++)
     {
-        GraphBase *graph = graph_engine.create_graph_handle();
+        GraphBase *graph = graph_engine->create_graph_handle();
         InCursor *in_cursor = graph->get_innbd_iter();
         adjlist found = {0};
 
-        in_cursor->set_key_range(graph_engine.get_key_range(i));
+        in_cursor->set_key_range(graph_engine->get_key_range(i));
 
         in_cursor->next(&found);
 
@@ -53,9 +53,9 @@ int64_t BUStep(const GraphEngine &graph_engine,
                 {
                     if (front.get_bit(v))
                     {
-                        parent[u] = v;
+                        parent[found.node_id] = v;
                         awake_count++;
-                        next.set_bit(u);
+                        next.set_bit(found.node_id);
                         break;
                     }
                 }
@@ -68,7 +68,7 @@ int64_t BUStep(const GraphEngine &graph_engine,
     return awake_count;
 }
 
-int64_t TDStep(const GraphEngine &graph_engine,
+int64_t TDStep(GraphEngine *graph_engine,
                pvector<node_id_t> &parent,
                SlidingQueue<node_id_t> &queue)
 {
@@ -76,12 +76,12 @@ int64_t TDStep(const GraphEngine &graph_engine,
 
 #pragma omp parallel
     {
-        QueueBuffer<NodeID> lqueue(queue);
+        QueueBuffer<node_id_t> lqueue(queue);
 #pragma omp for reduction(+ : scout_count) nowait
         for (auto q_iter = queue.begin(); q_iter < queue.end(); q_iter++)
         {
             node_id_t u = *q_iter;
-            GraphBase *graph = graph_engine.create_graph_handle();
+            GraphBase *graph = graph_engine->create_graph_handle();
             for (node_id_t v : graph->get_out_nodes_id(u))
             {
                 node_id_t curr_val = parent[v];
@@ -111,7 +111,7 @@ void QueueToBitmap(const SlidingQueue<node_id_t> &queue, Bitmap &bm)
     }
 }
 
-void BitmapToQueue(const GraphEngine &graph_engine,
+void BitmapToQueue(GraphEngine *graph_engine,
                    const Bitmap &bm,
                    SlidingQueue<node_id_t> &queue,
                    int thread_num)
@@ -122,11 +122,11 @@ void BitmapToQueue(const GraphEngine &graph_engine,
 #pragma omp for nowait
         for (int i = 0; i < thread_num; i++)
         {
-            GraphBase *graph = graph_engine.create_graph_handle();
+            GraphBase *graph = graph_engine->create_graph_handle();
             NodeCursor *node_cursor = graph->get_node_iter();
             node found = {0};
 
-            node_cursor->set_key_range(graph_engine.get_key_range(i));
+            node_cursor->set_key_range(graph_engine->get_key_range(i));
 
             node_cursor->next(&found);
 
@@ -142,20 +142,20 @@ void BitmapToQueue(const GraphEngine &graph_engine,
     queue.slide_window();
 }
 
-pvector<node_id_t> InitParent(const GraphEngine &graph_engine, int thread_num)
+pvector<node_id_t> InitParent(GraphEngine *graph_engine, int thread_num)
 {
-    GraphBase *graph_stat = graph_engine.create_graph_handle();
+    GraphBase *graph_stat = graph_engine->create_graph_handle();
     pvector<node_id_t> parent(graph_stat->get_num_nodes());
     graph_stat->close();
 
 #pragma omp parallel for num_threads(thread_num)
     for (int i = 0; i < thread_num; i++)
     {
-        GraphBase *graph = graph_engine.create_graph_handle();
+        GraphBase *graph = graph_engine->create_graph_handle();
         NodeCursor *node_cursor = graph->get_node_iter();
         node found = {0};
 
-        node_cursor->set_key_range(graph_engine.get_key_range(i));
+        node_cursor->set_key_range(graph_engine->get_key_range(i));
 
         node_cursor->next(&found);
 
@@ -171,13 +171,13 @@ pvector<node_id_t> InitParent(const GraphEngine &graph_engine, int thread_num)
     return parent;
 }
 
-pvector<NodeID> DOBFS(const GraphEngine &graph_engine,
-                      node_id_t source,
-                      int alpha = 15,
-                      int beta = 18,
-                      int thread_num)
+pvector<node_id_t> DOBFS(GraphEngine *graph_engine,
+                         node_id_t source,
+                         int alpha = 15,
+                         int beta = 18,
+                         int thread_num = 1)
 {
-    GraphBase *graph_stat = graph_engine.create_graph_handle();
+    GraphBase *graph_stat = graph_engine->create_graph_handle();
     pvector<node_id_t> parent = InitParent(graph_engine, thread_num);
     parent[source] = source;
     SlidingQueue<node_id_t> queue(graph_stat->get_num_nodes());
@@ -201,17 +201,18 @@ pvector<NodeID> DOBFS(const GraphEngine &graph_engine,
             do
             {
                 old_awake_count = awake_count;
-                awake_count = BUStep(g, parent, front, curr, thread_num);
+                awake_count =
+                    BUStep(graph_engine, parent, front, curr, thread_num);
                 front.swap(curr);
             } while ((awake_count >= old_awake_count) ||
-                     (awake_count > g.num_nodes() / beta));
-            BitmapToQueue(g, front, queue, thread_num);
+                     (awake_count > graph_stat->get_num_nodes() / beta));
+            BitmapToQueue(graph_engine, front, queue, thread_num);
             scout_count = 1;
         }
         else
         {
             edges_to_check -= scout_count;
-            scout_count = TDStep(g, parent, queue);
+            scout_count = TDStep(graph_engine, parent, queue);
             queue.slide_window();
         }
     }
@@ -222,4 +223,34 @@ pvector<NodeID> DOBFS(const GraphEngine &graph_engine,
 
     graph_stat->close();
     return parent;
+}
+
+int main(int argc, char *argv[])
+{
+    cout << "Running BFS" << endl;
+    CmdLineApp bfs_cli(argc, argv);
+    if (!bfs_cli.parse_args())
+    {
+        return -1;
+    }
+
+    graph_opts opts;
+    opts.create_new = bfs_cli.is_create_new();
+    opts.is_directed = bfs_cli.is_directed();
+    opts.read_optimize = bfs_cli.is_read_optimize();
+    opts.is_weighted = bfs_cli.is_weighted();
+    opts.optimize_create = bfs_cli.is_create_optimized();
+    opts.db_name = bfs_cli.get_db_name();  //${type}_rd_${ds}
+    opts.db_dir = bfs_cli.get_db_path();
+    std::string bfs_log = bfs_cli.get_logdir();  //$RESULT/$bmark
+    opts.stat_log = bfs_log + "/" + opts.db_name;
+    opts.conn_config = "cache_size=10GB";  // bfs_cli.get_conn_config();
+    opts.type = bfs_cli.get_graph_type();
+
+    const int THREAD_NUM = 1;
+    GraphEngine::graph_engine_opts engine_opts{.num_threads = THREAD_NUM,
+                                               .opts = opts};
+
+    GraphEngine graphEngine(engine_opts);
+    DOBFS(&graphEngine, 0);
 }
