61c0f9b070cc24f78a3cfa471cba5866f8bf856d
apricot2012 <haotiangong@hotmail.com>
2022-08-22 15:50:19 -0700

    Added shell scripts for SSSP

--- benchmark/benchmark_definitions.h
+++ benchmark/benchmark_definitions.h
@@ -37,6 +37,12 @@ typedef struct cc_info
     cc_info(int _val) : component_count(_val), time_taken(_val){};
 } cc_info;
 
+typedef struct sssp_info
+{
+    double time_taken;
+    sssp_info(int _val) : time_taken(_val){};
+} sssp_info;
+
 typedef struct pr_map
 {
     int id;
