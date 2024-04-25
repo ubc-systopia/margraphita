873e9e7271d81c3d7363316e2831d199a2cf7fdc
Puneet Mehrotra <p.puneetmehrotra@outlook.com>
2024-04-25 10:36:01 -0700

    added VTune profiling to iteration_vc. Also, begin changes on edgelist to make it split edgekey

--- benchmark/iteration_vc.cpp
+++ benchmark/iteration_vc.cpp
@@ -1,99 +1,101 @@
-#include <math.h>
-#include <stdio.h>
-#include <unistd.h>
-
-#include <algorithm>
-#include <cassert>
-#include <deque>
-#include <fstream>
 #include <iostream>
-#include <queue>
-#include <set>
-#include <vector>
+#include <unordered_set>
 
-#include "adj_list.h"
 #include "benchmark_definitions.h"
 #include "command_line.h"
 #include "common_util.h"
-#include "csv_log.h"
-#include "edgekey.h"
 #include "graph_engine.h"
-#include "graph_exception.h"
-#include "platform_atomics.h"
-#include "pvector.h"
 #include "standard_graph.h"
 #include "times.h"
+#ifdef DEBUG
+#include <ittnotify.h>
+__itt_domain* domain = __itt_domain_create("MyTraces.MyDomain");
+__itt_string_handle* InScan = __itt_string_handle_create("InNbd Scan Task");
+__itt_string_handle* OutScan = __itt_string_handle_create("OutNbd Scan Task");
+
+#endif
+#define ITERS 50
 
 int main(int argc, char* argv[])
 {
-    std::cout << "Running Iteration" << std::endl;
+    cpu_set_t mask;
+    CPU_ZERO(&mask);
+    CPU_SET(0, &mask);
+    sched_setaffinity(0, sizeof(mask), &mask);
+    std::cout << "Testing Neighborhood Iteration" << std::endl;
     CmdLineApp iter_cli(argc, argv);
     if (!iter_cli.parse_args())
     {
         return -1;
     }
 
-    graph_opts opts;
-    get_graph_opts(iter_cli, opts);
-    opts.stat_log = iter_cli.get_logdir() + "/" + opts.db_name;
+    cmdline_opts opts = iter_cli.get_parsed_opts();
+    opts.stat_log += "/" + opts.db_name;
 
     const int THREAD_NUM = 1;
-    GraphEngine::graph_engine_opts engine_opts{.num_threads = THREAD_NUM,
-                                               .opts = opts};
-
-    int num_trials = iter_cli.get_num_trials();
-
     Times t;
     t.start();
-    GraphEngine graphEngine(engine_opts);
+    GraphEngine graphEngine(THREAD_NUM, opts);
     graphEngine.calculate_thread_offsets();
     t.stop();
-    std::cout << "Graph loaded in " << t.t_micros() << std::endl;
-
-    for (int i = 0; i < num_trials; i++)
+    std::cout << "Graph loaded in " << t.t_secs() << std::endl;
+    std::unordered_set<node_id_t> nodes;
+    std::unordered_set<node_id_t> in_nodes;
+    for (int i = 0; i < opts.num_trials; i++)
     {
-        uint64_t counter = 0;
         iter_info info(0);
-        t.start();
-#pragma omp parallel for num_threads(THREAD_NUM)
-        for (int i = 0; i < THREAD_NUM; i++)
+        for (int id = 0; id < THREAD_NUM; id++)
         {
             GraphBase* graph = graphEngine.create_graph_handle();
-            adjlist found = {0};
+            adjlist found;
+            t.start();
 
+            __itt_task_begin(domain, __itt_null, __itt_null, InScan);
             OutCursor* out_cursor = graph->get_outnbd_iter();
-            out_cursor->set_key_range(graphEngine.get_key_range(i));
+            out_cursor->set_key_range(graphEngine.get_key_range(id));
             out_cursor->next(&found);
             while (found.node_id != -1)
             {
+                nodes.insert(found.node_id);
                 for (node_id_t v : found.edgelist)
                 {
-                    counter += 1;
+                    nodes.insert(v);
                 }
+                found.clear();
                 out_cursor->next(&found);
             }
+            __itt_task_end(domain);
 
-            if (opts.is_directed)
+            t.stop();
+            info.time_taken = t.t_secs();
+            std::cout << "Iteration over out edges completed in : "
+                      << info.time_taken << std::endl;
+            out_cursor->close();
+
+            found.clear();
+            t.start();
+            __itt_task_begin(domain, __itt_null, __itt_null, InScan);
+            InCursor* in_cursor = graph->get_innbd_iter();
+            in_cursor->set_key_range(graphEngine.get_key_range(id));
+            in_cursor->next(&found);
+            while (found.node_id != -1)
             {
-                InCursor* in_cursor = graph->get_innbd_iter();
-                in_cursor->set_key_range(graphEngine.get_key_range(i));
-                in_cursor->next(&found);
-                while (found.node_id != -1)
+                in_nodes.insert(found.node_id);
+                for (node_id_t v : found.edgelist)
                 {
-                    for (node_id_t v : found.edgelist)
-                    {
-                        counter += 1;
-                    }
-                    in_cursor->next(&found);
+                    in_nodes.insert(v);
                 }
+                found.clear();
+                in_cursor->next(&found);
             }
+            __itt_task_end(domain);
+            t.stop();
+            info.time_taken = t.t_secs();
+            std::cout << "Iteration over in edges completed in : "
+                      << info.time_taken << std::endl;
+            std::cout << "number of in-nodes: " << in_nodes.size() << std::endl;
             graph->close();
         }
-        t.stop();
-        info.time_taken = t.t_micros();
-        std::cout << "Iteration over all edges completed in : "
-                  << info.time_taken << std::endl;
-        print_csv_info(opts.db_name, info, iter_cli.get_logdir());
     }
 
     return 0;
